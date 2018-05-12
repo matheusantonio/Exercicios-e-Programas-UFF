a603b9385bd2391ed56b17f1a8c998b8a222fb3a
matheus antonio <matheus_antonio22@hotmail.com>
2018-05-12 11:52:42 -0300

    Adicionados alguns comentários.

--- PAQ/T2PAQ/main.c
+++ PAQ/T2PAQ/main.c
@@ -6,14 +6,17 @@
 
 int main() {
 
+    //Le o arquivo texto que sera usado para implementar o algoritmo
     FILE *arq = fopen("arquivo.txt", "rb");
 
+    //Tipo fila que armazenara os bytes lidos e a quantidade de vezes que eles aparecem
     fila contagem = fila_Inicializar();
 
     uint8_t num;
 
     while(!feof(arq))
     {
+        //Para cada byte lido, aidiciona ele a fila
         fread(&num, 1, 1, arq);
         fila_Inserir(contagem, num);
     }
@@ -22,13 +25,16 @@ int main() {
 
     fila_Imprimir(contagem);
 
+    //Arvore onde serao armazenadas as frequencias de cada byte para implementacao do algoritmo
     raiz huff = ini_Arvore();
 
+    //Funcao para inserir as duas primeiras frequencias
     insereRaizPrimeiro(huff, elempos(contagem, 0), elempos(contagem, 1));
 
     int i;
     for(i=2; i<getTop(contagem); i++)
     {
+        //Insercao do resto das frequencias
         inserir_Arvore(huff, elempos(contagem, i));
     }
 
