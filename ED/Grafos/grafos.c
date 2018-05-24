55ca3193d9859460783aae88ad4da8e45fd9909f
matheus antonio <matheus_antonio22@hotmail.com>
2018-05-24 12:54:40 -0300

    Trabalho de grafos finalizado.

--- ED/Grafos/grafos.c
+++ ED/Grafos/grafos.c
@@ -10,152 +10,239 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct aresta
+typedef struct Grafo
 {
     int destino;
     float custo;
-    struct aresta *prox;
-} Aresta;
-
-typedef struct grafo
-{
-    Aresta **a;
+    struct Grafo *prox;
 } Grafo;
 
-Grafo *iniGrafo(int n)
+Grafo ** iniGrafo(int n)
 {
-    Grafo *g = malloc(sizeof(struct grafo));
-    g->a = malloc(n * sizeof(struct aresta));
+    Grafo **g=(Grafo**)malloc((n+1)*sizeof(Grafo*));
     int i;
-    for(i=0;i<n;i++)
+    for(i=0;i<=n;i++)
     {
-        g->a[i] = NULL;
+        g[i] = NULL;
     }
     return g;
 }
 
-int buscarAresta(Grafo *g, int origem, int destino)
+Grafo* inserir(Grafo *g, int origem, int destino, float custo)
 {
-    Aresta *aux = g->a[origem];
-    while(aux!=NULL)
+    Grafo *novo = (Grafo*)malloc(sizeof(Grafo));
+    novo->destino=destino;
+    novo->custo=custo;
+    if(g == NULL)
     {
-        if(aux->destino==destino)
+        g = novo;
+        g->prox=NULL;
+    }
+    else
+    {
+        novo->prox = g;
+        g = novo;
+    }
+    return g;
+}
+
+Grafo* remover(Grafo *g, int destino)
+{
+    Grafo *busca = g, *aux=NULL;
+    if(g->prox == NULL)
+    {
+        free(g);
+        g = NULL;
+        return g;
+    }
+    while(busca->destino != destino)
+    {
+        aux = busca;
+        busca = busca->prox;
+    }
+    aux->prox = busca->prox;
+    free(busca);
+    return g;
+}
+
+void imprimir(Grafo **g, int n)
+{
+    int i;
+    for(i=1;i<=n;i++)
+    {
+        printf("%d-> ", i);
+        Grafo *aux = g[i];
+        while(aux!=NULL)
+        {
+            printf("(%d:%.2f), ", aux->destino, aux->custo);
+            aux = aux->prox;
+        }
+        printf("\n");
+    }
+}
+
+int procurar(Grafo *g, int destino)
+{
+    while(g!=NULL)
+    {
+        if(g->destino == destino)
             return 1;
-        aux=aux->prox;
+        g = g->prox;
     }
     return 0;
 }
 
-void inserirAresta(Grafo *g, int origem, int destino, float custo)
+int contGrafo(Grafo *g, int origem)
 {
-    Aresta *a = malloc(sizeof(struct aresta));
-    a->destino = destino;
-    a->custo = custo;
-    a->prox = NULL;
-    printf("%d %d %f\n", origem, a->destino, a->custo);
-    if(g->a[origem] == NULL)
+    int cont=0;
+    while(g!=NULL)
     {
-        g->a[origem] = a;
+        if(g->destino == origem)
+            cont++;
+        g = g->prox;
     }
-    else
-    {
-        Aresta *aux = g->a[origem];
-        while(aux->prox!=NULL) aux = aux->prox;
-        aux->prox = a;
-    }
+    return cont;
 }
 
-void removerAresta(Grafo *g, int origem, int destino)
+int contGrafo2(Grafo *g)
 {
-    Aresta *aux = g->a[origem], *del = NULL;
-    while(aux!=NULL)
+    int cont=0;
+    while(g!=NULL)
     {
-        if(aux->destino==destino)
-        {
-            if(del==NULL)
-                g->a[origem] = aux->prox;
-            else
-                del->prox = aux->prox;
-            free(aux);
-            return;
-        }
-        del = aux;
-        aux = aux->prox;
+        cont++;
+        g = g->prox;
     }
+    return cont;
 }
 
-void imprimirGrafo(Grafo *g, int n)
+int grauVertice(Grafo **g, int origem, int n)
 {
-    Aresta *aux;
+    int cont=0;
+    cont = contGrafo2(g[origem]);
     int i;
-    for(i=1;i<n;i++)
+    for(i=0;i<=n;i++)
     {
-        aux = g->a[i];
-        printf("%d: ", i);
+        if(i!=origem)
+        {
+            cont = cont + contGrafo(g[i], origem);
+        }
+    }
+    return cont;
+}
+
+int espaco(Grafo **g, int n)
+{
+    Grafo *aux;
+    int cont = 0, i;
+    for(i=1;i<=n;i++)
+    {
+        aux = g[i];
         while(aux!=NULL)
         {
-            printf("%d|%.2f, ", g->a[i]->destino, g->a[i]->custo);
+            cont++;
             aux=aux->prox;
         }
-        printf("\n");
     }
+    int tot = n*(n-1);
+    if((cont*100/tot) > 30)
+        return 0;
+    else
+        return 1;
+}
+
+Grafo* destruirAresta(Grafo *g)
+{
+    if(g!=NULL)
+    {
+        destruirAresta(g->prox);
+        free(g);
+        return NULL;
+    }
+    return NULL;
+}
+
+Grafo ** destruir(Grafo **g, int n)
+{
+    int i;
+    for(i=1;i<=n;i++)
+        g[i]=destruirAresta(g[i]);
+    free(g);
+    g = NULL;
+    return g;
 }
 
 int main()
 {
     int n, op=0, origem, destino;
     float custo;
-    printf("Indique o numero de nos do grafo:\n");
+    printf("Quantos nos tem o grafo?\n");
     scanf("%d", &n);
-    n++;
-    Grafo *g = iniGrafo(n);
+
+    Grafo **g = iniGrafo(n);
+
+    g[1] = inserir(g[1], 1, 3, 2.5);
+    g[1] = inserir(g[1], 1, 2, 3.5);
+    g[2] = inserir(g[2], 2, 5, 1.5);
+    g[3] = inserir(g[3], 3, 4, 1);
+    g[3] = inserir(g[3], 3, 1, 6);
+    g[4] = inserir(g[4], 4, 3, 1.75);
+    g[5] = inserir(g[5], 5, 1, 2.5);
+    g[5] = inserir(g[5], 5, 2, 1.4);
+    g[5] = inserir(g[5], 5, 4, 2.25);
 
     while(op!=6)
     {
         system("cls");
-        printf("1)Inserir aresta\n2)Remover aresta\n3)Imprimir grafo\n4)Calcular grau de um vertice\n5)Verificar se eh espaco\n6)Sair\n");
+        printf("1)Inserir aresta\n2)Remover aresta\n3)Imprimir grafo\n4)Grau de um no\n5)Verifica se grafo eh espaco\n6)Sair\n");
         scanf("%d", &op);
         switch(op)
         {
         case 1:
-            printf("Insira origem:");
+            printf("Insira origem: ");
             scanf("%d", &origem);
-            printf("Insira destino:");
+            printf("Insira destino: ");
             scanf("%d", &destino);
-            if(buscarAresta(g, origem, destino))
+            if(procurar(g[origem], destino))
             {
-                printf("Essa aresta ja existe!\n");
+                printf("Aresta ja existe!");
                 break;
             }
-            printf("Insira custo:");
+            printf("Insira o custo: ");
             scanf("%f", &custo);
-            inserirAresta(g, origem, destino, custo);
+            g[origem] = inserir(g[origem], origem, destino, custo);
             break;
         case 2:
-            printf("Insira origem:");
+            printf("Insira origem: ");
             scanf("%d", &origem);
-            printf("Insira destino:");
+            printf("Insira destino: ");
             scanf("%d", &destino);
-            if(!buscarAresta(g, origem, destino))
+            if(!procurar(g[origem], destino))
             {
-                printf("Essa aresta nao existe!\n");
+                printf("Aresta nao existe!");
                 break;
             }
-            removerAresta(g, origem, destino);
+            g[origem] = remover(g[origem], destino);
             break;
         case 3:
-            imprimirGrafo(g, n);
+            imprimir(g, n);
             break;
         case 4:
+            printf("Indique a oridem: ");
+            scanf("%d", &origem);
+            printf("Esse vertice tem grau %d.",grauVertice(g, origem, n));
             break;
         case 5:
+            if(espaco(g, n))
+                printf("Grafo eh espaco.\n");
+            else
+                printf("Grafo nao eh espaco.\n");
             break;
         case 6:
+            g = destruir(g, n);
             break;
         }
         fflush(stdin);
         getchar();
     }
-
     return 0;
 }
