e18484a2861e652de0362791aec9b3172c9d1233
Matheus Antonio <matheus.antonio.c@outlook.com>
2018-06-06 20:44:30 -0300

    Adicionao trabalho de estruturas genericas.

---
+++ ED/Estruturas Genericas/genericas.c
@@ -0,0 +1,15 @@
+//================================================================
+// Faca um programa que manipule uma lista generica de veiculos:
+// Carro-> renavam, placa, modelo e potencia
+// Moto-> Placa, quilometragem, cor 
+// 1) Inserir um veiculo 
+// 2) Remover um veiculo (pela placa)
+// 3) Imprimir a lista 
+// 4) Contar as motos de uma determinada cor 
+// 5) Imprime o carro de maior potencia 
+// 6) Sair e destruir 
+//================================================================
+
+
+#include<stdio.h>
+#include<stdlib.h>
