6a44321a9ddcb13036c1a838898e2a19990cb9f8
Matheus Antonio <matheus.antonio.c@outlook.com>
2018-06-11 22:22:17 -0300

    Adicionada estrutura para armazenar os dados da lista.

--- PAQ/T3PAQ/main.c
+++ PAQ/T3PAQ/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct _funcionario{
+    char Nome[15];
+    char Sobrenome[15];
+    char Cidade[30];
+    char Estado[30];
+    char Data_Nascimento[8];
+    char Empresa[30];
+    char Estado_Civil[10];
+    float Salario;
+} Funcionario;
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
 }
